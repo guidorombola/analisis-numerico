62f59270f027080f9e06a6ab9ab47db1b9cda0a7
Guido Rombolá <guidorombola@gmail.com>
2019-07-24 10:49:39 -0300

    Cambia forma de expresar problema de flujo maximo

--- final/codigos-final/pulp_maximo_flujo.py
+++ final/codigos-final/pulp_maximo_flujo.py
@@ -5,18 +5,17 @@ x13 = LpVariable("x13", 0, 15)  # 0<= x13 <= 15
 x23 = LpVariable("x23", 0, 4)  # 0<= x23 <= 4
 x24 = LpVariable("x24", 0, 10)  # 0<= x24 <= 10
 x34 = LpVariable("x34", 0, 18)  # 0<= x34 <= 18
-v = LpVariable("v")
 
 # Definimos el problema (Pudiendo ser de maximizacion o minimizacion)
 prob = LpProblem("Max flow", LpMaximize)
 
 # Definimos las restricciones
-prob += x12 + x13 == v
+
 prob += x12 - x23 - x24 == 0
 prob += x13 + x23 - x34 == 0
 
 # Definimos la funcion objetivo
-prob += v
+prob += x12 + x13
 
 # Resolvemos el problema
 status = prob.solve(pulp.GLPK())
@@ -27,7 +26,7 @@ print(x13.value())
 print(x23.value())
 print(x24.value())
 print(x34.value())
-print(v.value())
+
 
 print('Valor optimo: ', pulp.value(prob.objective))
 
